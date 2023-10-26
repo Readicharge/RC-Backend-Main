c8180967e5033114030083a80aef073035962299
Readicharge <Readicharge@gmail.com>
2023-10-26 22:45:47 +0530

    material Price Determiner WIP

--- RC-MAIN/RC-BUSINESS-LOGIC/RC-CUSTOMER/RC-CUSTOMER-OPERATIONS/RC-CUSTOMER-QUOTATION/RC-QUOTE-MAIN/rc-material-list-determiner-from-questions.js
+++ RC-MAIN/RC-BUSINESS-LOGIC/RC-CUSTOMER/RC-CUSTOMER-OPERATIONS/RC-CUSTOMER-QUOTATION/RC-QUOTE-MAIN/rc-material-list-determiner-from-questions.js
@@ -1,7 +1,20 @@
-
+// Importing the Mateial Model 
+const Material = require("../../../../../RC-CORE/RC-CONFIG-CORE/models/RC-MATERIAL/rc-material-model");
 
 const get_material_list = async (req,res) => {
-// 
+
+// Getting the Question List 
+const {question_list} = req.body;
+
+// Taking the all materials from the Material List 
+const all_materials = await Material.find({});
+
+console.log(all_materials);
+
+
+res.status(200).json(all_materials)
+
+
 
 }
 
