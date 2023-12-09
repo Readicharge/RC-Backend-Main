843c93506c908a8349de476c755e96eda49cd67a
Readicharge <Readicharge@gmail.com>
2023-12-09 20:11:20 +0530

    Update rc-job-routes-main.js

--- RC-MAIN/RC-BUSINESS-LOGIC/RC-JOB-CORE/rc-job-routes-main.js
+++ RC-MAIN/RC-BUSINESS-LOGIC/RC-JOB-CORE/rc-job-routes-main.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 
-const {rc_job_creater,rc_job_Installer_confirmator,get_job_By_customerId,get_specfic_job_id,cancelJobByInstaller} = require("./rc-job-core");
+const {rc_job_creater,rc_job_Installer_confirmator,get_job_By_customerId,get_specfic_job_id,cancelJobByInstaller,cancelJobByCustomer} = require("./rc-job-core");
 router.post("/create-new",rc_job_creater);
 router.post("/update-job-to-confirm-installer",rc_job_Installer_confirmator);
 router.post("/get-job-by-customerId",get_job_By_customerId);
