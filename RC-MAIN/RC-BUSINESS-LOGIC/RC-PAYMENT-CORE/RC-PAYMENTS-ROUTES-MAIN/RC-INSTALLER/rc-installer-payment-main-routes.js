4f25e000c2d8715dcc97557b19409f016f16401f
Readicharge <Readicharge@gmail.com>
2023-12-20 14:45:35 +0530

    Update rc-installer-payment-main-routes.js

--- RC-MAIN/RC-BUSINESS-LOGIC/RC-PAYMENT-CORE/RC-PAYMENTS-ROUTES-MAIN/RC-INSTALLER/rc-installer-payment-main-routes.js
+++ RC-MAIN/RC-BUSINESS-LOGIC/RC-PAYMENT-CORE/RC-PAYMENTS-ROUTES-MAIN/RC-INSTALLER/rc-installer-payment-main-routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 const {hold_payment_for_subscription} = require("../../RC-PAYMENT-OPERATIONS/RC-INSTALLER/RC-SUBSCRIPTION-PAYMENTS/rc-hold-subscription-amount");
-router.post("/installerPayment1", hold_payment_for_subscription);
+router.post("/installerPayment1/:installerId", hold_payment_for_subscription);
 
 
 
