349d7849287a9837beb957e6aa3fa58ff86335e3
Readicharge <Readicharge@gmail.com>
2023-12-29 00:18:17 +0530

    ....

--- RC-MAIN/RC-BUSINESS-LOGIC/RC-PAYMENT-CORE/RC-PAYMENTS-ROUTES-MAIN/RC-INSTALLER/rc-installer-payment-main-routes.js
+++ RC-MAIN/RC-BUSINESS-LOGIC/RC-PAYMENT-CORE/RC-PAYMENTS-ROUTES-MAIN/RC-INSTALLER/rc-installer-payment-main-routes.js
@@ -5,7 +5,16 @@ const router = express.Router();
 
 
 const {hold_payment_for_subscription} = require("../../RC-PAYMENT-OPERATIONS/RC-INSTALLER/RC-SUBSCRIPTION-PAYMENTS/rc-hold-subscription-amount");
+const { 
+    addBankAccount,
+    updateBankAccount,
+    deleteBankAccount
+    } = require("../../RC-PAYMENT-OPERATIONS/RC-INSTALLER/RC-ACCOUNT-MAKER/rc-account-maker");
+
 router.post("/installerPayment1/:installerId", hold_payment_for_subscription);
+router.post("/installerPayment2/:installerId", addBankAccount);
+router.put("/installerPayment3/:installerId", updateBankAccount);
+router.delete("/installerPayment4/:installerId", deleteBankAccount);
 
 
 
